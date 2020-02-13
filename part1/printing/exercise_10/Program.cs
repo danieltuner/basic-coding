2205a06cc2a863afe9b7c95a72ac0c84a132b2d8
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-13 13:02:46 +0200

    exercise 10 done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -7,6 +7,16 @@ namespace exercise_10
         public static void Main(string[] args)
         {
             // Write your code here:
+            Console.WriteLine("I will tell a story, but I need some information.");
+            Console.WriteLine("Give a name for main character:");
+            string message = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            string message2 = Console.ReadLine();
+            Console.WriteLine("Here is the story:");
+            Console.WriteLine("Once upon a time there was a " + message2 + " called " + message);
+            Console.WriteLine("On her way to work, " + message + " often pondered what being " + message2 + " meant to them.");
+            Console.WriteLine("When you work as a " + message2 + " you meet interesting people.");
+            Console.WriteLine(message + " enjoys their work as " + message2 + ", The end.");
 
         }
     }
