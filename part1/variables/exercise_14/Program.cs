0fb77ab1e4856dedd415be595659dd5a57c7775d
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-14 10:39:05 +0200

    exercise 14 done

--- part1/variables/exercise_14/Program.cs
+++ part1/variables/exercise_14/Program.cs
@@ -8,9 +8,8 @@ namespace exercise_14
     {
       Console.WriteLine("Give me the truth!");
       // Write your code here:
-      string truth = "True";
       string message = Console.ReadLine();
-      bool booleanValue = System.Convert.ToBoolean(truth);
+      bool booleanValue = System.Convert.ToBoolean(message);
       Console.WriteLine(booleanValue);
 
     }
