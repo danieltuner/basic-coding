f4706f52b7ce01447e7cedb5bbb1c8a0364e1cbf
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-14 11:51:11 +0200

    exercise 15 done

--- part1/variables/exercise_15/Program.cs
+++ part1/variables/exercise_15/Program.cs
@@ -8,7 +8,21 @@ namespace exercise_15
     {
 
       // Write your code here:
-
+      Console.WriteLine("Give a string:");
+      string UserInput1 = Console.ReadLine();
+      Console.WriteLine("Give an integer:");
+      string UserInput2 = Console.ReadLine();
+      int intValue = Convert.ToInt32(UserInput2);
+      Console.WriteLine("Give a double:");
+      string UserInput3 = Console.ReadLine();
+      double doubleValue = Convert.ToDouble(UserInput3);
+      Console.WriteLine("Give a boolean:");
+      string UserInput4 = Console.ReadLine();
+      bool booleanValue = System.Convert.ToBoolean(UserInput4);
+      Console.WriteLine("Your string: " + UserInput1);
+      Console.WriteLine("Your integer: " + intValue);
+      Console.WriteLine("Your double: " + doubleValue);
+      Console.WriteLine("Your boolean: " + booleanValue);
     }
   }
 }
