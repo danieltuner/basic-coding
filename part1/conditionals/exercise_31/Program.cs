3b5b258583849f4bc9086ca7dc4243b2e9680836
Daniel Tunér <tuner.daniel@hotmail.com>
2020-02-17 16:09:54 +0200

    ex 31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -7,6 +7,17 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string input1 = Console.ReadLine();
+      int number1 = Convert.ToInt32(input1);
+      if ((number1 % 2) == 0)
+      {
+        Console.WriteLine("It is even.");
+      }
+      else
+      {
+        Console.WriteLine("It is odd.");
+      }
 
     }
   }
