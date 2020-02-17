007037d733dcc3a5d1f6dbf9042d0058e60cb1ad
Daniel Tunér <tuner.daniel@hotmail.com>
2020-02-17 14:20:33 +0200

    ex 26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_26
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give your age:");
+      string input = Console.ReadLine();
+      int number = Convert.ToInt32(input);
+      if (number <= 1899) 
+  {
+      Console.WriteLine("You're old");
+  }
     }
   }
 }
