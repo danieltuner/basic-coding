d362385f8a409bffbeb1fe5d66690283a9632b9e
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-19 10:30:05 +0200

    ex 42 done

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -8,6 +8,13 @@ namespace exercise_42
     {
 
       // Write your code here:
+      int first = 1;
+      int second = 2;
+      first = Convert.ToInt32(Console.ReadLine());
+      second = Convert.ToInt32(Console.ReadLine());
+      int sum = first + second;
+      double squareRoot = Math.Sqrt(sum);
+      Console.WriteLine(squareRoot);
     
     }
   }
