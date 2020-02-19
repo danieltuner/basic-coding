76611560cb47a8758aa679c6557ad6ac1b3a4bf2
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-19 10:35:26 +0200

    ex 43 done

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_43
     public static void Main(string[] args)
     {
       // Write your code here:
-      
+      int value = 1;
+      value = Convert.ToInt32(Console.ReadLine());
+      if (value < 0)
+      {
+        Console.WriteLine(value * -1);
+      }
+      else
+      {
+        Console.WriteLine(value);
+      }
     }
   }
 }
