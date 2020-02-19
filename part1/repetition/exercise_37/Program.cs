54d7340604728dd68a34b08f297731ff718c1317
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-19 09:36:37 +0200

    ex 37 done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -8,6 +8,21 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number == 0)
+        {
+          break;
+        }
+        else
+        {
+          sum ++;
+        }
+      }
+        Console.WriteLine("Total amount of numbers: " + sum);      
 
     }
   }
