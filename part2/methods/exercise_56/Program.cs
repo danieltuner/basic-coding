30b941dbf7c48a9e9640ebb0227b0a8e5aa7742d
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-20 12:46:41 +0200

    ex 56 done peh

--- part2/methods/exercise_56/Program.cs
+++ part2/methods/exercise_56/Program.cs
@@ -6,10 +6,15 @@ namespace exercise_56
   {
     public static void Main(String[] args)
     {
-
+      string text = Word();
+      Console.WriteLine(text);
     }
 
     // Write your method here:
+    public static string Word()
+    {
+      return ("Tjofaderittan lej!");
+    }
 
   }
 }
