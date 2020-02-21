25590d17a39a7a876449cde32c0de2a93fade749
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-21 11:50:35 +0200

    ex 61 done

--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -6,27 +6,56 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
+    PrintRightTriangle(4);
+    Console.WriteLine("");
+    ChristmasTree(4);
+    Console.WriteLine("");
+    ChristmasTree(10);
+      
 
     }
 
     public static void PrintStars(int number)
     {
-
+      for (int i = 0; i < number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
     }
 
     public static void PrintSpaces(int number)
     {
-
+      for (int i = 0; i < number; i++)
+      {
+        Console.Write(" ");
+      }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+      int space = size - 1;
+      for (int i = 1; i <= size; i++)
+      {
+        PrintSpaces(space);
+        PrintStars(i);
+        space--;
+      }
+      
     }
 
     public static void ChristmasTree(int height)
     {
-
+      
+      for (int i = 1; i <= height; i++)
+      {
+        PrintSpaces(height - i);
+        PrintStars((i * 2) -1);
+      }
+      PrintSpaces(height - 2);
+      PrintStars(3);
+      PrintSpaces(height - 2);
+      PrintStars(3);
     }
   }
 }
