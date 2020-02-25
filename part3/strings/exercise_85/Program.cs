6a33f95c65e9ca344064563507dbbedd16e9cbda
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-25 12:42:30 +0200

    ex 85 done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
-
+      Console.WriteLine("Enter username:");
+      string input1 = Console.ReadLine();
+      Console.WriteLine("Enter password:");
+      string input2 = Console.ReadLine();
+      if (input1 == "alex" && input2 == "sunshine")
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else if (input1 == "emma" && input2 == "haskell")
+      {
+        Console.WriteLine("You have successfully logged in!");
+      }
+      else
+      {
+        Console.WriteLine("Incorrect username or password!");
+      }
 
     }
   }
