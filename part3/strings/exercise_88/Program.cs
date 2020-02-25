f073202bf9d203f7dd8012e7926fb18fcd3007ea
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-25 13:27:23 +0200

    ex 88 done

--- part3/strings/exercise_88/Program.cs
+++ part3/strings/exercise_88/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_88
   {
     public static void Main(string[] args)
     {
-
+      while(true)
+      {
+      string text = Console.ReadLine();
+      if (text == "")
+      {
+        break;
+      }
+      string[] pieces = text.Split(" ");
+      {
+        Console.WriteLine(pieces[0]);
+      }
+      }
     }
   }
 }
