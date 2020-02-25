7b47dbb137bb42f51080385974b1d0570a429223
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-25 13:18:15 +0200

    ex 87 done

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
-
+      while(true)
+      {
+        string text = Console.ReadLine();
+        if (text == "")
+        {
+          break;
+        }
+        string[] pieces = text.Split(" ");
+        for (int i = 0; i < pieces.Length; i++)
+        {
+          if (pieces[i].Contains("av"))
+          { 
+            Console.WriteLine(pieces[i]);
+          }
+        }
+      }
     }
 
   }
