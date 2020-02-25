aa85d3320eb942b2b1a4534a28513a7d56dd64de
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-25 14:48:03 +0200

    ex 92 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,7 +7,28 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
-
+      int oldest = Convert.ToInt32(DateTime.Now.Year);
+      string name = "";
+      while(true)
+      {
+        string text = Console.ReadLine();
+        if (text == "")
+        {
+          break;
+        }
+        string[] pieces = text.Split(",");
+        if (oldest > Convert.ToInt32(pieces[1]))
+        {
+          oldest = Convert.ToInt32(pieces[1]);
+        }
+        if (name.Length < pieces[0].Length)
+        {
+          name = pieces[0];
+        }
+        
+      }
+      Console.WriteLine("Longest name: " + name);
+      Console.WriteLine("Highest age: " + (Convert.ToInt32(DateTime.Now.Year) - oldest));
     }
   }
 }
