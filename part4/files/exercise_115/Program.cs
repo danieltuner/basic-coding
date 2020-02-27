e266430c09c8351de12ba8ff0d88b0841cc3aa72
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-27 12:13:05 +0200

    ex 115 done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -6,7 +6,19 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
+      
 
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                int number = Convert.ToInt32(input);
+                Console.WriteLine(number * number * number);
+            }
+            
     }
   }
 }
