7a2acde17393bcb4d31c10f6124a04db24f72065
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-27 13:54:13 +0200

    ex 120 done

--- part5/revision/exercise_120/Program.cs
+++ part5/revision/exercise_120/Program.cs
@@ -7,6 +7,8 @@ namespace exercise_120
     public static void Main(string[] args)
     {
       // You can test your cube here.
+      Cube theCube = new Cube(2);
+      Console.WriteLine(theCube);
       
     }
   }
