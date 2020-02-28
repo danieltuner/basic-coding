939ac35380cdf5454f15b2f1b513440006fa1c2a
Daniel Tunér <daniel.tuner@centria.fi>
2020-02-28 13:13:24 +0200

    ex 126 done

--- part5/references/exercise_126/Program.cs
+++ part5/references/exercise_126/Program.cs
@@ -7,6 +7,21 @@ namespace exercise_126
     public static void Main(string[] args)
     {
       // Try your code here, if you want
+      PaymentTerminal lunchCafeteria = new PaymentTerminal();
+
+double change = lunchCafeteria.DrinkCoffee(10);
+Console.WriteLine("remaining change: " + change);
+
+PaymentCard annesCard = new PaymentCard(15);
+
+bool wasSuccessful = lunchCafeteria.EatLunch(annesCard);
+Console.WriteLine("there was enough money: " + wasSuccessful);
+wasSuccessful = lunchCafeteria.EatLunch(annesCard);
+Console.WriteLine("there was enough money: " + wasSuccessful);
+wasSuccessful = lunchCafeteria.DrinkCoffee(annesCard);
+Console.WriteLine("there was enough money: " + wasSuccessful);
+
+Console.WriteLine(lunchCafeteria);
     }
   }
 }
