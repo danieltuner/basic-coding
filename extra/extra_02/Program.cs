eb31385fbcb3ffa19e79ca005bc8369e76261332
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-03-11 09:00:59 +0200

    ex 2 done

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -7,6 +7,22 @@ namespace extra_02
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give three numbers:");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      int num3 = Convert.ToInt32(Console.ReadLine());
+      if (num1 > num2 && (num2 > num3))
+      {
+        Console.WriteLine("Largest: " + num1);
+      }
+      if (num2 > num1 && (num1 > num3))
+      {
+        Console.WriteLine("Largest: " + num2);
+      }
+      if (num3 > num1 && (num1 > num2))
+      {
+        Console.WriteLine("Largest: " + num3);
+      }
 
     }
   }
