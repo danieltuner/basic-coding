87c5f62235d11514fbc3cd2e8367f409778b1e3b
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-03-12 10:00:56 +0200

    ex 5 done

--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_05
 {
@@ -7,6 +8,29 @@ namespace extra_05
     public static void Main(string[] args)
     {
       // Add your code here:
+      for (int i = 1; i < 6; i++)
+      {
+        Console.WriteLine(i);
+      }
+      Console.WriteLine("Give words, 'end' quits:");
+
+      List<string> word = new List<string>();
+      string words = "";
+      
+      while (true)
+      {
+        words = Console.ReadLine();
+        if (words == "end")
+        {
+          break;
+        }
+        else{
+          word.Add(words);
+        }
+        
+
+      }
+      word.ForEach(Console.WriteLine);
 
     }
   }
