f8f472f7415e5929edc4fecb54042cc9536c8e62
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-03-12 10:59:28 +0200

    not yet

--- extra/extra_07/Program.cs
+++ extra/extra_07/Program.cs
@@ -7,7 +7,24 @@ namespace extra_07
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give integers, 'end' quits:");
+      string end = "";
+      int sum = 0;
+      
+      
+      while (true)
+        
+        if (end == "end")
+        {
+          break;
+        }
+        else
+        {
+          int input = Convert.ToInt32(Console.ReadLine());
+          sum = input + sum;
 
+        }
+      Console.WriteLine("Sum: " + sum);
     }
   }
 }
