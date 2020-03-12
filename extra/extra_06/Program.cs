401d96ed0e96556dda8697e5ee27028710938fef
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-03-12 10:18:38 +0200

    ex 6 done

--- extra/extra_06/Program.cs
+++ extra/extra_06/Program.cs
@@ -7,7 +7,18 @@ namespace extra_06
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      int first = Convert.ToInt32(Console.ReadLine());
+      int second = Convert.ToInt32(Console.ReadLine());
+      int sum = first + second;
+      if (sum < 0)
+      {
+        Console.WriteLine("0");
+      }
+      else
+      {
+        Console.WriteLine(Math.Sqrt(sum));
+      }
+      
     }
   }
 }
