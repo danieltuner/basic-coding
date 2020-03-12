4c3c36b72b7e03547ac1f9b6a8b3d10a417d7a91
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-03-12 08:54:02 +0200

    something wrong

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -7,6 +7,31 @@ namespace extra_03
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("How many numbers?");
+      int input = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give " + input + " numbers:");
+      int times = 0;
+      int count = 0;
+      int sum = 0;
+      while (true)
+      {
+        count = Convert.ToInt32(Console.ReadLine());
+        times++;
+        if (times == input)
+        {
+          break;
+        }
+        else
+        {
+          sum = count + sum;
+        }
+      
+      }
+      double avarage = sum / times;
+      
+      Console.WriteLine("Therir sum: " + sum);
+      Console.WriteLine("Their total: " + count);
+      Console.WriteLine("Their avarage: " + avarage);
 
     }
   }
