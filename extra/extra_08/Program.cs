37cbb7778983936d69ad8c6b6a69220614d16357
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-03-12 12:18:51 +0200

    weird

--- extra/extra_08/Program.cs
+++ extra/extra_08/Program.cs
@@ -7,7 +7,35 @@ namespace extra_08
     public static void Main(string[] args)
     {
       // Add your code here:
+      ChristmasTree(4);
 
+    }
+    public static void PrintStars(int nr)
+    {
+      for (int i = 0; i < nr; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
+    }
+    public static void PrintSpaces(int nr)
+    {
+      for (int i = 0; i < nr; i++)
+      {
+        Console.WriteLine(" ");
+      }
+    }
+    public static void ChristmasTree(int height)
+    {
+      for (int i = 1; i <= height; i++)
+      {
+        PrintSpaces(height -i);
+        PrintStars((i* 2) -1);
+      }
+      PrintSpaces(height -2);
+      PrintStars(3);
+      PrintSpaces(height -2);
+      PrintStars(3);
     }
   }
 }
