59ac1956681469cf1520e627400d4ab497fa6b8d
Daniel Tunér <tuner.daniel@hotmail.com>
2020-03-23 15:29:57 +0200

    empty

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -2,31 +2,8 @@
 
     namespace sandbox
     {
-        public class Item
-    {
-    private string name;
-
-    public Item(string name) 
-    {
-        this.name = name;
-    }
     
-    // In C# this could also be done with { get; set; for the varible}
-    // That way the variable would be public
-    // For this example, we want to protect it and keep it private
-    public string GetName()
     {
-        return this.name;
-    }
-
-    public string SetName(string name) 
-    {
-        this.name = name;
-    }
-
-    public override string ToString()
-    {
-        return this.name;
-    }
+    
     }
     }
