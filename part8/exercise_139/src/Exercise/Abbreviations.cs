b6290327152219bb24248b4d5f4e15368ff2b78b
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-10 18:23:20 +0300

    ex 139 done

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,39 @@ namespace Exercise
 {
   public class Abbreviations
   {
+    private Dictionary<string, string> förkortning;
 
+    public Abbreviations()
+    {
+      this.förkortning = new Dictionary<string, string>();
+    }
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.förkortning.Add(abbreviation, explanation);
     }
 
     public bool HasAbbreviation(string abbreviation)
     {
-      return false;
+      if (this.förkortning.ContainsKey(abbreviation))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if (HasAbbreviation(abbreviation))
+      {
+        return förkortning[abbreviation];
+      }
+      else
+      {
+        return "not found";
+      }
     }
   }
 }
