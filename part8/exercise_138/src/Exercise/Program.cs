19ba02eae1adb77b23f82995fe5c5c07d5746275
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-10 18:03:04 +0300

    ex 138 done

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -8,8 +8,16 @@ namespace Exercise
     public static void Main(string[] args)
     {
       // Do something here
+      Dictionary<string, string> nicknames = new Dictionary<string, string>();
+      nicknames.Add("matthew", "matt");
+      nicknames.Add("michael", "mix");
+      nicknames.Add("arthur", "artie");
 
+      foreach (KeyValuePair<string, string> kvp in nicknames)
+      {
+        Console.WriteLine("{0}'s nickname is {1}", kvp.Key, kvp.Value);
       }
+      
     }
   }
 }
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -2,7 +2,7 @@
 
     namespace sandbox
     {
-    
+                  
     {
     
     }
