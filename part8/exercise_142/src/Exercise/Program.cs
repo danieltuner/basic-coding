71de26e0aa744b337e8e65a5e226f007d7e55210
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-13 15:22:06 +0300

    ex 142 done

--- part8/exercise_142/src/Exercise/Program.cs
+++ part8/exercise_142/src/Exercise/Program.cs
@@ -8,6 +8,10 @@ namespace Exercise
     public static void Main(string[] args)
     {
       IOU mattsIOU = new IOU();
+      mattsIOU.ChangeDebt("Arthur", -10);
+
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+
       mattsIOU.ChangeDebt("Arthur", 51);
       mattsIOU.ChangeDebt("Arthur", 30);
 
