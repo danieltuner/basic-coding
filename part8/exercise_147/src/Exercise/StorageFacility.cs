2a96a0737e2fc0c839436793b83a206bc9de0f7a
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-14 16:16:58 +0300

    ex 147 done

--- part8/exercise_147/src/Exercise/StorageFacility.cs
+++ part8/exercise_147/src/Exercise/StorageFacility.cs
@@ -5,24 +5,49 @@ namespace Exercise
 {
   public class StorageFacility
   {
+    private Dictionary<string, List<string>> storageUnits;
+    
+    public StorageFacility()
+    {
+      this.storageUnits = new Dictionary<string, List<string>>();
+    }
 
     public void Add(string unit, string item)
     {
-
+      if (!this.storageUnits.ContainsKey(unit))
+      {
+        this.storageUnits.Add(unit, new List<string>());
+      }
+      this.storageUnits[unit].Add(item);
     }
 
     public List<string> Contents(string storageUnit)
     {
-      return new List<string>();
+      if (!this.storageUnits.ContainsKey(storageUnit))
+      {
+        this.storageUnits.Add(storageUnit, new List<string>());
+      }
+      return this.storageUnits[storageUnit];
     }
 
     public void Remove(string storageUnit, string item)
     {
-
+      this.storageUnits[storageUnit].Remove(item);
+      if (this.storageUnits[storageUnit].Count == 0)
+      {
+        this.storageUnits.Remove(storageUnit);
+      }
     }
 
     public List<string> StorageUnits()
     {
-      return new List<string>();
+      List<string> listStorageUnits = new List<string>();
+      Dictionary<string, List <string>>.KeyCollection key = this.storageUnits.Keys;
+      foreach (string storageUnits in key)
+      {
+        listStorageUnits.Add(storageUnits);
+      }
+      return listStorageUnits;
     }
   }
+}
