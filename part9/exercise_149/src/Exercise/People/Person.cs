ee3534194742532ca6b74c9e026fedb8046b5fa6
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-18 10:42:32 +0300

    ex 149 done

--- part9/exercise_149/src/Exercise/People/Person.cs
+++ part9/exercise_149/src/Exercise/People/Person.cs
@@ -2,5 +2,18 @@ namespace Exercise
 {
   public class Person
   {
+    public string name {get; set;}
+    public string adress {get; set;}
+
+    public Person(string name, string adress)
+    {
+      this.name = name;
+      this.adress = adress;
+    }
+
+    public override string ToString()
+    {
+      return this.name + ", " + this.adress;
+    }
   }
 }
--- part9/exercise_149/src/Exercise/People/Teacher.cs
+++ part9/exercise_149/src/Exercise/People/Teacher.cs
@@ -1,6 +1,15 @@
 namespace Exercise
 {
-  public class Teacher
+  public class Teacher : Person
   {
+    public int salary { get; set;}
+    public Teacher(string name, string adress, int salary) : base(name, adress)
+    {
+      this.salary = salary;
+    }
+    public override string ToString()
+    {
+      return base.ToString() + " salary " + this.salary + " per month";
+    }
   }
 }
