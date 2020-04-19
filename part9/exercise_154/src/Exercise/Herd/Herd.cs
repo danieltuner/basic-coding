daae2c76f9370926a502c24b2a838039861764d9
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-19 17:21:12 +0300

    ex 154 done

--- part9/exercise_154/src/Exercise/Herd/Herd.cs
+++ part9/exercise_154/src/Exercise/Herd/Herd.cs
@@ -3,22 +3,34 @@ namespace Exercise
   using System.Collections.Generic;
   public class Herd : IMovable
   {
+    private List<IMovable> herd;
 
     public Herd()
     {
+      this.herd = new List<IMovable>();
     }
 
-    public void AddToHerd(IMovable m)
+    public void AddToHerd(IMovable movable)
     {
+      this.herd.Add(movable);
     }
 
     public void Move(int dx, int dy)
     {
+      foreach (IMovable movable in herd)
+      {
+        movable.Move(dx, dy);
+      }
     }
 
     public override string ToString()
     {
-      return "";
+      string herdPositions = "";
+      foreach (IMovable movable in herd)
+      {
+        herdPositions += movable.ToString() + "\n";
+      }
+      return herdPositions;
     }
   }
 }
--- part9/exercise_154/src/Exercise/Herd/Organism.cs
+++ part9/exercise_154/src/Exercise/Herd/Organism.cs
@@ -2,18 +2,22 @@ namespace Exercise
 {
   public class Organism : IMovable
   {
-
+    public int x;
+    public int y;
     public Organism(int x, int y)
     {
+      this.x = x;
+      this.y = y;
     }
     public void Move(int dx, int dy)
     {
-
+      this.x += dx;
+      this.y += dy;
     }
 
     public override string ToString()
     {
-      return "";
+      return "x: " + this.x + "; y: " + this.y;
     }
   }
 }
