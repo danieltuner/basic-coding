6e46572d105c655f3bbcd91972f13952963c11a9
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-26 21:23:50 +0300

    ex 160 done

--- part10/exercise_160/src/Exercise/CardGame/Hand.cs
+++ part10/exercise_160/src/Exercise/CardGame/Hand.cs
@@ -5,23 +5,57 @@ namespace Exercise
   public class Hand : IComparable<Hand>
   {
     private List<Card> hand;
+    public Hand()
+    {
+      this.hand = new List<Card>();
+    }
 
     public void Add(Card card)
     {
+      if (!this.hand.Contains(card))
+      {
+        this.hand.Add(card);
+      }
     }
 
     public void Print()
     {
+      foreach (Card card in this.hand)
+      {
+        Console.WriteLine(card.ToString());
+      }
     }
 
     public void Sort()
     {
+      this.hand.Sort();
     }
 
 
     public int CompareTo(Hand hand)
     {
-      return 0;
+      int sumOfHand1 = 0;
+      foreach (Card card in this.hand)
+      {
+        sumOfHand1 += card.value;
+      }
+      int sumOfHand2 = 0;
+      foreach (Card card in hand.hand)
+      {
+        sumOfHand2 += card.value;
+      }
+      if (sumOfHand1 == sumOfHand2)
+      {
+        return 0;
+      }
+      else if ( sumOfHand1 > sumOfHand2)
+      {
+        return 1;
+      }
+      else
+      {
+        return -1;
+      }
     }
   }
 }
