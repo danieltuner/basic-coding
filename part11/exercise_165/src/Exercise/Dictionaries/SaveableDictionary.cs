b9ebee0452ea959eafd7b91b6681e082c225801b
Daniel Tunér <tuner.daniel@hotmail.com>
2020-04-29 12:23:03 +0300

    ex 165 done

--- part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
+++ part11/exercise_165/src/Exercise/Dictionaries/SaveableDictionary.cs
@@ -1,39 +1,98 @@
 namespace Exercise
 {
-  using System;
-  using System.IO;
-  using System.Collections.Generic;
-  public class SaveableDictionary
-  {
-    public SaveableDictionary()
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    public class SaveableDictionary
     {
-    }
+        private Dictionary<string, string> dict;
+        private string file;
+        public SaveableDictionary()
+        {
+            this.dict = new Dictionary<string, string>();
+        }
 
-    public SaveableDictionary(string file) : this()
-    {
-    }
+        public SaveableDictionary(string file) : this()
+        {
+            this.file = file;
+        }
 
-    public void Add(string word, string translation)
-    {
-    }
+        public void Add(string word, string translation)
+        {
+          if (!this.dict.ContainsKey(word))
+          {
+            this.dict.Add(word, translation);
+            this.dict.Add(translation, word);
+          }
+        }
 
-    public bool Load()
-    {
-      return false;
-    }
+        public bool Load()
+        {
+          try
+          {
+            string[] lines = File.ReadAllLines(this.file);
+            foreach (string line in lines)
+            {
+              string[] parts = line.Split(":");
+              string word = parts[0];
+              string translation = parts[1];
+              Add(word, translation);
+            }
+            
+          }
+          catch (Exception e)
+            {
+              Console.WriteLine(e.Message);
+              return false;
+            }
+          return true;
+        }
 
-    public bool Save()
-    {
-      return false;
-    }
+        public bool Save()
+        {
+          List<string> alreadySaved = new List<string>();
+          try
+          {
+            StreamWriter sw = new StreamWriter(this.file);
+            foreach (string word in this.dict.Keys)
+              {
+                string composition = word + ":" + this.dict[word];
+                string backwards = this.dict[word] + ":" + word;
+                if (!alreadySaved.Contains(composition) && !alreadySaved.Contains(backwards))
+                {
+                  alreadySaved.Add(composition);
+                  sw.WriteLine(composition);
+                }
+              }
+            sw.Close();
+            
+          }
+          catch (Exception e)
+          {
+            Console.WriteLine(e.Message);
+            return false;
+          }
+          return true;
 
-    public string Translate(string word)
-    {
-      return null;
-    }
+        }
 
-    public void Delete(string word)
-    {
+        public string Translate(string word)
+        {
+          if (this.dict.ContainsKey(word))
+          {
+            return this.dict[word];
+          }
+          return null;
+        }
+
+        public void Delete(string word)
+        {
+          if (this.dict.ContainsKey(word))
+          {
+            string translation = this.dict[word];
+            this.dict.Remove(word);
+            this.dict.Remove(translation);
+          }
+        }
     }
-  }
 }
